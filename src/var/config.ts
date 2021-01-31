3d5f1e795f345e60734af4046ba000b4b0721f25
oliver021 <oliver021val@gmail.com>
2021-01-31 17:43:18 -0500

    set the online mongo server

--- src/var/config.ts
+++ src/var/config.ts
@@ -2,5 +2,5 @@ export const PORT: number = 3000
 export const ROUTES_DIR: string = './dist/routes/**/*.js'
 export const MODELS_DIR: string = './dist/models/**/*.js'
 export const USE_DB: boolean = true
-//export const DB_CONNECTION_STRING = 'mongodb://ufmadsbvz2fzuovj7vrc:kFTByDQYdE91dUOnRzuY@bw7thiaflgavms8-mongodb.services.clever-cloud.com:27017/bw7thiaflgavms8';
-export const DB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/local';
+export const DB_CONNECTION_STRING = 'mongodb://ufmadsbvz2fzuovj7vrc:kFTByDQYdE91dUOnRzuY@bw7thiaflgavms8-mongodb.services.clever-cloud.com:27017/bw7thiaflgavms8';
+//export const DB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/local';
